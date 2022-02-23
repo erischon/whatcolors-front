797f7bc84205174443044f325e0b6e4f91a401ff
Eri Schon <erischon@gmail.com>
2022-02-23 15:37:40 +0100

    Update ColorPanel.js

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,34 +1,16 @@
 import React, { Component } from "react"
 import "./ColorPanel.css"
 
-class ColorPanel extends Component {
-    constructor () {
-        super()
-    }
-        
-    listItems = () => {
-        const colorPanel = this.props
-        const listItems = colorPanel.map((color) => {
-            <li>Couleur : { color.hex } Pourcentage: { color.value }</li>
-        })
-        return (
-            console.log(listItems),
-            <ul>{ listItems }</ul>
-        )
-    } 
 
-    render () {
-        return (
-            <div className="colorpanel__container">
-            <ul>
-                {
-                    this.listItems()
-                }
-            </ul>
-            </div>
-        )
-    }
-    }
+const ColorPanel = ({ colorPanel }) => {
+    return (
+        <div className="colorpanel__container">
+            <h1>Panel</h1>
+            <p>{ colorPanel[0] }</p>
+            <p>Hop</p>
+        </div>
+    )
+}
 
 
 export default ColorPanel
