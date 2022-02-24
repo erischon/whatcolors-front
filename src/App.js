9144fd656343ea85785705450cfe35017a5b5400
Eri Schon <erischon@gmail.com>
2022-02-24 17:58:43 +0100

    app css

--- src/App.js
+++ src/App.js
@@ -110,13 +110,17 @@ class App extends Component {
         <Navigation isSignedIn={ isSignedIn} onRouteChange={ this.onRouteChange }/>
         { route === 'home' 
           ? <div className="main__container">
-            <Rank 
-              name={this.state.user.name}
-              entries={this.state.user.entries}
-            />
-            <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={ this.onButtonSubmit } />
-            <DisplayImage imageUrl={ imageUrl } />
-            <ColorPanel colorPanel={ colorPanel } />
+              <Rank 
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
+              <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={ this.onButtonSubmit } />
+
+              <div className="result__container container grid">
+                <DisplayImage imageUrl={ imageUrl } />
+                <ColorPanel colorPanel={ colorPanel } />
+              </div>
+
           </div>
           : (
             route === 'signin'
