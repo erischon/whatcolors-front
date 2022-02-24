b283c98a4f3f6438c8fdcfd016227e348196af69
Eri Schon <erischon@gmail.com>
2022-02-24 17:38:02 +0100

    ImageLinkForm css

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,21 +1,21 @@
 import React from "react"
-
+import "./ImageLinkForm.css"
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
-        <div>
-            <p className="text-lg mb-6">
-                { "This Magic Brain will Detect faces in your pictures.Give it a try" }
-            </p>
-            <div className="flex justify-center w-6/12 p-4 drop-shadow-xl rounded-lg">
-                <input className="text-xl p-2 w-9/12 mx-auto" type="tex" onChange={ onInputChange }/>
+        <section className="ilf__container container grid">
+            <div className="ilf_subtitle">
+                { "This Tool will Detect Colors in your pictures. Give it a try!" }
+            </div>
+            <div className="ilf__form">
+                <input className="ilf__form--input" type="tex" onChange={ onInputChange }/>
                 <button 
-                    className="w-3/12 text-xl px-3 py-2 inline-block white bg-purple-700" 
+                    className="ilf__form--button" 
                     onClick={ onButtonSubmit }
                 >
                     Detect</button>
             </div>
-        </div>
+        </section>
     )
 }
 
