2aec857a68d243308a4f0618c536c2a949bfbc5b
Eri Schon <erischon@gmail.com>
2022-02-25 10:29:14 +0100

    footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./Footer.css"
-import es_logo from "./logo-es-classic-fff-2.jpg"
+import es_logo from "./logo-es-transparent-fff-1.png"
 
 const Footer = () => {
     return (
         <section className='footer__container container grid'>
-            <img src={ es_logo } alt="logo" className="footer__logo"/>
-            <span className='footer__copy'>&#169 Eri Schön All rights reserved</span>
+        <span className='footer__copy'>© Eri Schön All rights reserved</span>
+        <img src={ es_logo } alt="logo" className="footer__logo"/>
         </section>
     )
 }
