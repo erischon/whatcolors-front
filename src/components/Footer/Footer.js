f2b998591ec2ef7c03cff81f9906b87248e82a4c
Eri Schon <erischon@gmail.com>
2022-03-22 12:18:11 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import es_logo from "./logo-es-transparent-000-1.png"
 const Footer = () => {
     return (
         <section className='footer__container container grid'>
-        <span className='footer__copy'>© Eri Schön All rights reserved</span>
+        <span className='footer__copy'>&copy; { new Date().getFullYear() } Eri Schön All rights reserved</span>
         <img src={ es_logo } alt="logo" className="footer__logo"/>
         </section>
     )
