52067bfffe8ac64e31d799ef23d137eff071be92
Eri Schon <erischon@gmail.com>
2022-03-25 17:16:34 +0100

    Update ColorPanel.js

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,5 @@
 import React from "react"
-
-import "./ColorPanel.css"
-
+import { Fragment } from "react/cjs/react.production.min"
 
 const ColorPanel = ({ colorPanel }) => {
      
@@ -9,20 +7,25 @@ const ColorPanel = ({ colorPanel }) => {
         const divStyle = {
             backgroundColor: color.hex
         }
-        const pourcentage = Math.round(color.value * 100) 
+        const pourcentage = Math.round(color.value * 100)
+
         return (
-            <div className="colorpanel__item">
-                <span className="colorpanel__item--box" style={ divStyle  } />
-                <div className="colorpanel__item--name">{ color.name }</div>
-                <div className="colorpanel__item--hex">{ color.hex }</div>
-                <div className="colorpanel__item--hex">({ pourcentage }%)</div>
+            <div className="grid grid-cols-8 justify-start items-center gap-x-2 py-0.5">
+                <span className="inline-block w-5 h-5" style={ divStyle  } />
+                <div className="text-sm md:text-base col-span-2">{ color.hex }</div>
+                <div className="text-sm col-span-3">{ color.name }</div>
+                <div className="text-xs col-span-2">({ pourcentage }%)</div>
             </div>
         ) 
     })
 
     return (
-        <div className="colorpanel__container">
+        <div className="flex flex-col mx-3 justify-center">
             { listItem }
+
+            <div className="container mx-auto mt-10 flex justify-center">
+                <button className="text-white bg-teal-700 px-6 py-2 rounded">Export</button>
+            </div>
         </div>
     )
 }
