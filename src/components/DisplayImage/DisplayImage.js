fed1ce351c1e42cb5df0b01d01adb201ec3b33e4
Eri Schon <erischon@gmail.com>
2022-03-25 14:29:42 +0100

    Update DisplayImage.js

--- src/components/DisplayImage/DisplayImage.js
+++ src/components/DisplayImage/DisplayImage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import "./DisplayImage.css"
+// import "./DisplayImage.css"
 
 
 const DisplayImage = ({ imageUrl }) => {
     return (
-        <div className="displayimage__container">
+        <div className="">
             <div className="">
-                <img className="displayimage__image" id="inputImage" src={ imageUrl } alt="" height="auto"/>
+                <img className="w-5/5" id="inputImage" src={ imageUrl } alt="" height="auto"/>
             </div>       
         </div>
     )
