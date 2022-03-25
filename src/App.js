0cc6bb50b353e6a5a00fe4c14cb479b81aa814f9
Eri Schon <erischon@gmail.com>
2022-03-25 17:16:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
 
             <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={ this.onButtonSubmit } />
 
-            <div className="flex rounded p-2 container mx-auto bg-zinc-300">
+            <div className="md:grid md:grid-cols-2 items-start container mx-auto">
               <DisplayImage imageUrl={ imageUrl } />
               <ColorPanel colorPanel={ colorPanel } />
             </div>
