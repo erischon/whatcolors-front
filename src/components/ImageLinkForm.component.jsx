4123d55c175e2dc22ccdf408ba55d0bfa5e39c6c
Eri Schon <erischon@gmail.com>
2022-03-25 12:08:41 +0100

    Update ImageLinkForm.component.jsx

--- src/components/ImageLinkForm.component.jsx
+++ src/components/ImageLinkForm.component.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 // import "./ImageLinkForm.css"
 
+const button = "rounded outline outline-1 outline-teal-700 px-1 py-2 md:w-1/5 bg-teal-700 text-white"
+const buttonMedia = "md:hover:border-teal-700 md:hover:bg-white md:hover:text-teal-700"
+
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <section className="container mx-auto flex flex-col items-center my-2 py-2">
@@ -9,7 +12,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             </div>
             <div className="flex flex-col md:flex-row my-4 w-4/5 md:w-3/5 gap-y-2">
                 <input className="outline outline-1 outline-offset-1 rounded px-1 py-2 md:mr-2 md:w-4/5" placeholder="Image Url" type="tex" onChange={ onInputChange }/>
-                <button className="outline outline-1 outline-teal-700 rounded px-1 py-2 md:w-1/5 bg-teal-700 text-white md:hover:border-teal-700 md:hover:bg-white md:hover:text-teal-700" onClick={ onButtonSubmit }>Find</button>
+                <button className={ `${button} ${buttonMedia}` } onClick={ onButtonSubmit }>Find</button>
             </div>
         </section>
     )
