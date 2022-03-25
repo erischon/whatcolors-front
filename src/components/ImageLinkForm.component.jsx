fb263a4798b33ffa58e1849b501295c0fb6a930d
Eri Schon <erischon@gmail.com>
2022-03-25 12:49:39 +0100

    Update ImageLinkForm.component.jsx

--- src/components/ImageLinkForm.component.jsx
+++ src/components/ImageLinkForm.component.jsx
@@ -1,17 +1,26 @@
 import React from "react"
-// import "./ImageLinkForm.css"
 
-const button = "rounded outline outline-1 outline-teal-700 px-1 py-2 md:w-1/5 bg-teal-700 text-white"
-const buttonMedia = "md:hover:border-teal-700 md:hover:bg-white md:hover:text-teal-700"
+// CSS
+const section = "container mx-auto flex flex-col items-center my-2 py-2"
 
+const formContainer = "flex flex-col w-4/5 gap-y-2 my-4"
+const formContainerMedia = "md:flex-row md:w-3/5"
+
+const inputUrl = "rounded px-1 py-2 outline outline-1 outline-offset-1"
+const inputUrlMedia = "md:mr-2 md:w-4/5"
+
+const button = "bg-teal-700 text-white px-1 py-2 rounded outline outline-1 outline-teal-700"
+const buttonMedia = "md:hover:bg-white md:hover:text-teal-700 md:w-1/5 md:hover:border-teal-700"
+
+// Component
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
-        <section className="container mx-auto flex flex-col items-center my-2 py-2">
+        <section className={ section }>
             <div className="my-2">
                 { "Give us a direct link to a file on the web :" }
             </div>
-            <div className="flex flex-col md:flex-row my-4 w-4/5 md:w-3/5 gap-y-2">
-                <input className="outline outline-1 outline-offset-1 rounded px-1 py-2 md:mr-2 md:w-4/5" placeholder="Image Url" type="tex" onChange={ onInputChange }/>
+            <div className={ `${formContainer} ${formContainerMedia}` }>
+                <input className={ `${inputUrl} ${inputUrlMedia}` } placeholder="Image Url" type="tex" onChange={ onInputChange }/>
                 <button className={ `${button} ${buttonMedia}` } onClick={ onButtonSubmit }>Find</button>
             </div>
         </section>
